181a98d0d06f9467dd395708f6dfee2440a82193
Vedananda-19 <pvedananda@gmail.com>
2026-02-22 00:32:37 +0530

    Fixed Bug

--- my-frontend/src/App.jsx
+++ my-frontend/src/App.jsx
@@ -1,20 +1,27 @@
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useState } from "react";
+
 import Feed from "./pages/feed";
-import CreatePost from "./pages/createpost";
+import Createpost from "./pages/createpost";
+import Login from "./pages/login";
+import Register from "./pages/register";
+import Navbar from "./components/navbar";
 
 function App() {
+  const [loggedInUser, setLoggedInUser] = useState(null);
   const [posts, setPosts] = useState([]);
 
   return (
     <BrowserRouter>
+      <Navbar loggedInUser={loggedInUser} />
       <Routes>
         <Route path="/" element={<Feed posts={posts} />} />
         <Route
           path="/create"
-          element={<CreatePost posts={posts} setPosts={setPosts} />}
+          element={<Createpost posts={posts} setPosts={setPosts} loggedInUser={loggedInUser} />}
         />
+        <Route path="/login" element={<Login setLoggedInUser={setLoggedInUser} />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );

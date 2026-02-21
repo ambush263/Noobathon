a940711495c69047adbb57259f00f6e450d74df2
Vedananda-19 <pvedananda@gmail.com>
2026-02-21 18:58:31 +0530

    firebase-added

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,15 @@
 from flask import Flask,render_template,request, redirect, url_for
 import random
+import firebase_admin
+from firebase_admin import credentials, firestore
+import os
 
 app = Flask(__name__)
+
+cred = credentials.certificate(os.environ["GOOGLE_APPLICATION_CREDENTIALS"])
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+
 @app.route('/')
 def index():
     return 'Index Page'
